b1780b3d46cf7f49af8dc111d6bd1009e9b60b3a
binun <binun@cs.bgu.ac.il>
2017-05-21 15:42:35 +0300

    kvm trace issues

--- prepare.sh
+++ prepare.sh
@@ -10,8 +10,10 @@ apt-get build-dep qemu
 apt-get install -y python-pip git protobuf-compiler protobuf-c-compiler \
   libprotobuf-c0-dev libprotoc-dev libelf-dev \
   libcapstone-dev libdwarf-dev python-pycparser llvm-3.3 clang-3.3 libc++-dev
-#mkdir -p build-panda && cd build-panda
-#~/panda/panda/scripts/install_ubuntu.sh
-#ln /root/panda/panda/scripts/panda/build/x86_64-softmmu/qemu-system-x86_64 qemu
 
 #wget https://archives.fedoraproject.org/pub/archive/fedora/linux/releases/20/Live/x86_64/Fedora-Live-LXDE-x86_64-20-1.iso -O Fedora.iso 
+
+# https://access.redhat.com/documentation/en-US/Red_Hat_Enterprise_Linux/7/html/Virtualization_Deployment_and_Administration_Guide/sect-troubleshooting-systemtaptrace.html
+# https://vmsplice.net/~stefan/stefanha-tracing-summit-2014.pdf
+# https://events.linuxfoundation.org/images/stories/pdf/lcjp2012_guangrong.pdf
+# https://psuriset.com/2015/07/09/trace-system-calls-using-perf/
