e4452dbade6be460d573b6a1a64ef19c871aa05b
binun <binun@cs.bgu.ac.il>
2017-05-26 03:22:45 +0800

    ptrace

--- prepare.sh
+++ prepare.sh
@@ -19,3 +19,4 @@ apt-get install -y python-pip git protobuf-compiler protobuf-c-compiler \
 # https://psuriset.com/2015/07/09/trace-system-calls-using-perf/
 # https://ruinedsec.wordpress.com/2013/04/04/modifying-system-calls-dispatching-linux/
 #https://ruinedsec.wordpress.com/2013/04/04/modifying-system-calls-dispatching-linux/
+# PTRACE https://home.in.tum.de/~jelten/dynamic-syscall-translation.pdf
